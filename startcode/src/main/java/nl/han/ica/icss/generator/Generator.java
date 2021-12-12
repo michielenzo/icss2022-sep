979b99849dfe9b5b057d7bfb6fa706ebe491ab75
Michiel Idema <m.idema@textinfo.nl>
2021-12-12 17:34:44 +0100

    finished

--- startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
+++ startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
@@ -1,30 +1,66 @@
 package nl.han.ica.icss.generator;
 
 
-import nl.han.ica.icss.ast.AST;
-import nl.han.ica.icss.ast.ASTNode;
-import nl.han.ica.icss.ast.Literal;
+import nl.han.ica.datastructures.HANStack;
+import nl.han.ica.icss.ast.*;
 import nl.han.ica.icss.ast.literals.*;
+import nl.han.ica.icss.ast.selectors.ClassSelector;
+import nl.han.ica.icss.ast.selectors.IdSelector;
+import nl.han.ica.icss.ast.selectors.TagSelector;
 import nl.han.ica.icss.checker.VariableManager;
 
 public class Generator {
 
+
+	public static final String ASSIGNMENT_OPERATOR = ":=";
+	public static final String COLON = ":";
+	public static final String OPEN_BRACE = "{";
+	public static final String CLOSE_BRACE = "}";
 	VariableManager variableManager = new VariableManager();
+	private final HANStack<ASTNode> container = new HANStack<>();
+
+	public static final char SEMICOLON = ';';
+	public static final String SPACE = " ";
+	public static final String EOL = "\n";
 
 	public String generate(AST ast) {
-        return walkThroughASTRecursive(ast.root, "");
+        return walkASTRecursive(ast.root, "");
 	}
 
-	private String walkThroughASTRecursive(ASTNode node, String str){
+	private String walkASTRecursive(ASTNode node, String str){
 
 		String str2 = str;
 
-		if(node instanceof Literal){
-			str2 += getLiteralAsString((Literal) node);
+		if(node instanceof Literal && !(container.peek() instanceof VariableAssignment)){
+			str2 += getLiteralAsString((Literal) node) + SEMICOLON + EOL;
 		}
 
+		if(node instanceof Declaration){
+			str2 += SPACE + SPACE + ((Declaration) node).property.name + COLON + SPACE;
+		}
+
+		if(node instanceof ClassSelector) {
+			str2 += ((ClassSelector) node).cls + SPACE + OPEN_BRACE + EOL;
+		}
+
+		if(node instanceof IdSelector){
+			str2 += ((IdSelector) node).id + SPACE + OPEN_BRACE + EOL;
+		}
+
+		if(node instanceof TagSelector){
+			str2 += ((TagSelector) node).tag + SPACE + OPEN_BRACE + EOL;
+		}
+
+		container.push(node);
+
 		for (ASTNode child: node.getChildren()){
-			 str2 = walkThroughASTRecursive(child, str2);
+			 str2 = walkASTRecursive(child, str2);
+		}
+
+		container.pop();
+
+		if(node instanceof Stylerule){
+			str2 += CLOSE_BRACE + EOL;
 		}
 
 		return str2;
